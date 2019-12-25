723130a121e63b7086c2cc80be7e281306a42cdc
Theodore Omtzigt <theodore.omtzigt@ibm.com>
2019-12-25 18:20:47 -0500

    WIP: designing the CMake files

---
+++ applications/dsp/variance.cpp
@@ -0,0 +1,68 @@
+// variance.cpp: example program calculating the variance of a signal
+//
+// Copyright (C) 2017-2020 Stillwater Supercomputing, Inc.
+//
+// This file is part of the HPR-DSP project, which is released under an MIT Open Source license.
+
+// configure the number system to use
+#include <universal/posit/posit>
+// pull in HPR-BLAS
+#include <hprblas>
+
+/*
+
+Mathematical 	C++ Symbol	Decimal Representation
+Expression
+pi			M_PI		3.14159265358979323846
+pi/2			M_PI_2		1.57079632679489661923
+pi/4			M_PI_4		0.785398163397448309616
+1/pi			M_1_PI		0.318309886183790671538
+2/pi			M_2_PI		0.636619772367581343076
+2/sqrt(pi)		M_2_SQRTPI	1.12837916709551257390
+sqrt(2)			M_SQRT2		1.41421356237309504880
+1/sqrt(2)		M_SQRT1_2	0.707106781186547524401
+e			M_E		2.71828182845904523536
+log_2(e)		M_LOG2E		1.44269504088896340736
+log_10(e)		M_LOG10E	0.434294481903251827651
+log_e(2)		M_LN2		0.693147180559945309417
+log_e(10)		M_LN10		2.30258509299404568402
+
+*/
+
+constexpr double PI = 3.141592653589793238460;  // best practice for C++
+
+
+
+int main(int argc, char** argv)
+try {
+	using namespace std;
+	using namespace sw::unum;
+	using namespace sw::hprblas;
+
+	constexpr size_t nrSamples = 32;
+	int nrOfFailedTestCases = 0;
+
+	using Scalar = posit<32,2>;
+	using Samples = mtl::dense_vector<Scalar>;
+
+	Samples s1(nrSamples), s2(nrSamples), s3(nrSamples);
+
+	// calculate mean value of input signal
+	Scalar mean = sum(s1)/Scalar(nrSamples);
+	
+
+
+	return (nrOfFailedTestCases > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+catch (char const* msg) {
+	std::cerr << msg << std::endl;
+	return EXIT_FAILURE;
+}
+catch (std::runtime_error& err) {
+	std::cerr << err.what() << std::endl;
+	return EXIT_FAILURE;
+}
+catch (...) {
+	std::cerr << "Caught unknown exception" << std::endl;
+	return EXIT_FAILURE;
+}
---
+++ tools/cmake/HPR_BLASConfig.cmake
@@ -0,0 +1,10 @@
+message(STATUS "HPR_BLASConfig.cmake entered")
+message(STATUS " HPR_BLAS_DIR is set to ${HPR_BLAS_DIR}")
+
+set(HPR_BLAS_COMMON_CONFIG "${HPR_BLAS_DIR}/tools/cmake/HPR_BLASCommon.cmake")
+
+if( EXISTS ${HPR_BLAS_COMMON_CONFIG})
+	include(${HPR_BLAS_COMMON_CONFIG})
+else()
+	message(ERROR "  Could not find the HPR-BLAS library configuration at ${HPR_BLAS_COMMON_CONFIG}")
+endif()
