e3b89d442209207419b06fc568e400668a547123
Theodore Omtzigt <theodore.omtzigt@ibm.com>
2019-12-26 17:44:28 -0500

    removing blas from hpr-dsp

--- applications/dsp/variance.cpp
+++ applications/dsp/variance.cpp
@@ -9,29 +9,7 @@
 // pull in HPR-BLAS
 #include <hprblas>
 
-/*
-
-Mathematical 	C++ Symbol	Decimal Representation
-Expression
-pi				M_PI		3.14159265358979323846
-pi/2			M_PI_2		1.57079632679489661923
-pi/4			M_PI_4		0.785398163397448309616
-1/pi			M_1_PI		0.318309886183790671538
-2/pi			M_2_PI		0.636619772367581343076
-2/sqrt(pi)		M_2_SQRTPI	1.12837916709551257390
-sqrt(2)			M_SQRT2		1.41421356237309504880
-1/sqrt(2)		M_SQRT1_2	0.707106781186547524401
-e				M_E		2.71828182845904523536
-log_2(e)		M_LOG2E		1.44269504088896340736
-log_10(e)		M_LOG10E	0.434294481903251827651
-log_e(2)		M_LN2		0.693147180559945309417
-log_e(10)		M_LN10		2.30258509299404568402
-
-*/
-
 constexpr double PI = 3.141592653589793238460;  // best practice for C++
-
-
 
 int main(int argc, char** argv)
 try {
@@ -50,7 +28,7 @@ try {
 	Scalar twoPi = 2.0 * PI;
 	Scalar base = twoPi / Scalar(nrSamples);
 	for (int i = 0; i < nrSamples; i++) {
-		s1[i] = sin(Scalar(i) * base);
+		s1[i] = sin(Scalar(i) * base) + 1;
 	}
 
 	// calculate mean value of input signal
