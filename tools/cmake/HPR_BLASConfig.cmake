ac3145b87a2043af53b97c7d6763ab808610cc7d
Theodore Omtzigt <theodore.omtzigt@ibm.com>
2019-12-26 12:10:55 -0500

    fixed cmake process

--- tools/cmake/HPR_BLASConfig.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS " HPR_BLAS_DIR is ${HPR_BLAS_DIR}")
-
-set(HPR_BLAS_COMMON_CONFIG "${HPR_BLAS_DIR}/tools/cmake/HPR_BLASCommon.cmake")
-
-if( EXISTS ${HPR_BLAS_COMMON_CONFIG})
-	include(${HPR_BLAS_COMMON_CONFIG})
-else()
-	message(ERROR "  Could not find the HPR-BLAS library configuration at ${HPR_BLAS_COMMON_CONFIG}")
-endif()
